805abf0e23a74f545b60424e36418537a2997ae2
Sameer1903 <sameerkanade1903@gmail.com>
2024-07-26 13:49:13 +0530

    List-Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +12,17 @@ function App() {
         TS Amazon
       </header>
       <main>
-
+        <ul>
+          {sampleProducts.map((product) => {
+            return <li key={product.slug}>
+              <img src={product.image} alt={product.name} className='product-image' />
+              <h2>{product.name}</h2>
+              <p>₹{product.price}</p>
+            </li>
+          })}
+        </ul>
       </main>
-      <footer> 
+      <footer>
         All rights reserved
       </footer>
     </div>
