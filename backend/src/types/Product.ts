6c2a488c93a66ae043418eff1c93b4a4ccadcdb5
Sameer1903 <sameerkanade1903@gmail.com>
2024-07-29 18:30:30 +0530

    Create-Node-Server

---
+++ backend/src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+    name : string
+    slug : string //A slug is known as a URL slug is descriptive text after the website name at the end of a URL that identifies a web page on the domain.
+    image : string
+    category : string
+    brand : string
+    price : number
+    countInStock : number
+    description : string
+    rating : number
+    numReviews : number
+}
