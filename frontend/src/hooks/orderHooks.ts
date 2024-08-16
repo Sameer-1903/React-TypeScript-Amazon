ccb510fc72b689996f0b9f1e54921e3f80b206cc
Sameer1903 <sameerkanade1903@gmail.com>
2024-08-16 11:52:43 +0530

    Create-Order-Page

--- frontend/src/hooks/orderHooks.ts
+++ frontend/src/hooks/orderHooks.ts
@@ -1,24 +1,29 @@
-import { useMutation , UseMutationResult} from "@tanstack/react-query"
-import { CartItem, ShippingAddress } from "../types/Cart"
-import apiClient from "../apiClient"
-import { Order } from "../types/Order"
-
+import { useMutation, UseMutationResult, useQuery , UseQueryResult } from '@tanstack/react-query'
+import { CartItem, ShippingAddress } from '../types/Cart'
+import apiClient from '../apiClient'
+import { Order } from '../types/Order'
 
 export const useCreateOrderMutation = () =>
-              useMutation({
-                mutationFn: async (order: {
-                  orderItems: CartItem[]
-                  shippingAddress: ShippingAddress
-                  paymentMethod: string
-                  itemsPrice: number
-                  shippingPrice: number
-                  taxPrice: number
-                  totalPrice: number
-                }) =>
-                  (
-                    await apiClient.post<{ message: string; order: Order }>(
-                      `api/orders`,
-                      order
-                    )
-                  ).data,
-              })
+  useMutation({
+    mutationFn: async (order: {
+      orderItems: CartItem[]
+      shippingAddress: ShippingAddress
+      paymentMethod: string
+      itemsPrice: number
+      shippingPrice: number
+      taxPrice: number
+      totalPrice: number
+    }) =>
+      (
+        await apiClient.post<{ message: string; order: Order }>(
+          `api/orders`,
+          order
+        )
+      ).data,
+  })
+
+export const useGetOrderDetailsQuery = (id: string) =>
+  useQuery({
+    queryKey: ['orders', id],
+    queryFn: async () => (await apiClient.get<Order>(`api/orders/${id}`)).data,
+  })
